e287e2b8d70b9f594b9e339bcd95418cb2b28c76
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-11-09 19:59:10 +0300

    feat: implement more interactivity & video player

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const API_SECRET_KEY = 'apikey=609f836a';
+const API_URL = 'https://www.omdbapi.com/?'
+const POSTER_API_URL = 'http://img.omdbapi.com/?'
+
+const SEARCH_URL = API_URL + API_SECRET_KEY + '&s='
+
+
 
 fetch('http://www.omdbapi.com/?i=tt3896198&apikey=609f836a')
     .then((responce) => responce.json())
