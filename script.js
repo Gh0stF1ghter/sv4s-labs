5a0060ad6538c2dc4c2dcb25c4086256fff29efc
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-11-09 21:24:16 +0300

    feat: add dropdown menu sample

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const POSTER_API_URL = 'http://img.omdbapi.com/?'
 
 const SEARCH_URL = API_URL + API_SECRET_KEY + '&s='
 
-
-
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=609f836a')
-    .then((responce) => responce.json())
-    .then((movies) => console.log(movies))
-    .catch(error => console.log(error));
+// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=609f836a')
+//     .then((responce) => responce.json())
+//     .then((movies) => console.log(movies))
+//     .catch(error => console.log(error))
